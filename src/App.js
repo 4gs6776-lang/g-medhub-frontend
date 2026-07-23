6c833440245ad77c67d31a7598c6d1f00ec12c55
4gs6776-lang <4gs6776@gmail.com>
2026-07-23 05:09:37 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to G-MedHub</h1>
+      <p>Hospital Management System</p>
+      <p>Frontend is successfully running!</p>
+    </div>
+  );
+}
+
+export default App;
